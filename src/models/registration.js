0b7342b4b073f65202d157cc2ba6e1db61728c21
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-07-20 18:11:54 +0530

    dhgghgd

--- src/models/registration.js
+++ src/models/registration.js
@@ -296,7 +296,7 @@ const dealerRegistrationSchema = new mongoose.Schema({
 const userRegistrationSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    contact: { type: Number, required: true },
+    contact: { type: String, required: true },
     password: { type: String, required: true },
     address: { type: String  },
     role: { type: String, default: "USER" },
